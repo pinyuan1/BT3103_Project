48b14f4c26cbd3d64e681d8e2ccd38026b3024ba
pinyuan1 <50605411+pinyuan1@users.noreply.github.com>
2020-04-10 01:09:35 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,15 @@
-
 import Home from './components/Home.vue' 
 import AddItem from './components/AddItem.vue' //same name as the create new task file
 import Dashboard from './components/Dashboard.vue' //Same name as the dashboard file
 import Task from './components/Task.vue'
+import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
 
 export default [
     {path: '/', component: Home},
     {path: '/add', component: AddItem}, //change here
     {path: '/dashboard', component: Dashboard}, //change here
-    {path: '/task/:id', component:Task}
-
-import Login from './components/Login.vue'
-import Signup from './components/Signup.vue'
-export default[
-    {path:'/', component: Login},
+    {path: '/task/:id', component:Task},
+    {path:'/login', component: Login},
     {path:'/signup', component: Signup}
-
 ]
